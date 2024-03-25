356736e7ea81262d656ac1a6de235c388e16d898
raghavkaushik2004 <136466980+raghavkaushik2004@users.noreply.github.com>
2024-03-25 09:39:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
 capture = cv2.VideoCapture(0)
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 def detect(image) :
-    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2BGRAY)
-    face = face_classifier.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 9 )
-    if face is () : #check if empty tuple is returned
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face = classifier.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 9 )
+    if face is None : 
         return image
     for (x,y,w,h) in face :
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
